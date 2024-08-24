21b9d64ff69d1df1369d360137cec7da2f92d232
jasonic69 <justyhill3@hotmail.com>
2024-08-24 14:15:50 +0100

    2-get-topics complete

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const {getTopics,} = require('./controllers/topics-controllers');
+
+app.get('/api/topics', getTopics);
+
+app.all('/*',(req , res) => {
+    res.status(404).send({ msg: "Route Not Found" });
+})
+
+exports.handleServerErrors = (err, req, res, next) => {
+    res.status(500).send({ msg: "Internal Server Error" });
+};
+
+module.exports = app;
